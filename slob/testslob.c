17b5024927e0cdd9281f0656141163f7addf3387
Vitaly Osipov <vitaly.osipov@gmail.com>
2014-05-15 00:14:03 +1000

    better free space tracking, split patch into two

--- slob/testslob.c
+++ slob/testslob.c
@@ -2,13 +2,13 @@
 #include <sys/syscall.h>
 
 #define SLOB_REC 317
-#define SLOB_USED 318
+#define SLOB_FREE 318
 
 int main(int argc, char *argv[])
 {
 	int v1, v2;
 
 	v1 = syscall(SLOB_REC);
-	v2 = syscall(SLOB_USED);
-	printf ("SLOB memory reserved: %x usage: %.2f%%\n", v1, 100*(float)v2/(float)v1);
+	v2 = syscall(SLOB_FREE);
+	printf ("SLOB memory reserved: %x fragmentation %.2f%%\n", v1, 100*(float)v2/(float)v1);
 }
